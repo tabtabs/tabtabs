ce728eb695e76be7d3f46b9b04fd23afc4d390b2
namchanwoo <skacksdn00@naver.com>
2023-06-25 16:21:18 +0900

    몬스터에 노드가 스폰 될 위치를 정해주는 NodeArea 스크립트를 생성 - 이 클래스는 몬스터의 콜리전2D 또는 스파인바운더리 경계상자에 따라 영역을 생성하는 열거형을 정의했습니다. - 1행은 무조건 바운더리 하단에서 시작하며 행과 열을 정할 수 있습니다.
    몬스터에 기본으로 붙어야하는 클래스입니다.
    
    추 후 하드코딩 없이 자동으로 붙게 하는 방법 생각중..

---
+++ Assets/TabTabs/Scripts/Node/NodeArea.cs
@@ -0,0 +1,238 @@
+using System.Collections.Generic;
+
+using Spine;
+using Spine.Unity;
+using UnityEngine;
+
+using Sirenix.OdinInspector;
+
+namespace TabTabs.NamChanwoo
+{
+    public enum EAreaType
+    {
+        Collider2D,
+        SpineAnimation
+    }
+
+    public class NodeArea : MonoBehaviour
+    {
+       
+        [Title("Boundary Setting")]
+        [GUIColor(0.9f, 0.4f, 0.8f, 1f)]
+        [Required("반드시 BoxCollider2D를 넣어주세요.")]
+        public BoxCollider2D boxCollider2D;
+        
+        [GUIColor(0.9f, 0.4f, 0.8f, 1f)]
+        [Required("반드시 skeletonAnimation를 설정해주세요.")]
+        public SkeletonAnimation skeletonAnimation;
+        
+        [GUIColor(0.3f, 0.8f, 0.8f, 1f)]
+        [InfoBox("Collider2D는 BoxCollider2D의 크기를 기준으로 합니다.\n" + "SpineAnimation은 SkeletonAnimation의 크기를 기준으로 합니다.")]
+        [EnumToggleButtons, HideLabel]
+        [SerializeField] private EAreaType m_areaType = EAreaType.Collider2D;
+        
+        [GUIColor(0.3f, 0.8f, 0.8f, 1f)]
+        /*[MinValue(1),MaxValue(10)]*/
+        [ProgressBar(1, 10)]
+        [SerializeField] private int rows = 3;
+        
+        [GUIColor(0.3f, 0.8f, 0.8f, 1f)]
+        /*[MinValue(1),MaxValue(10)]*/
+        [ProgressBar(1, 10)]
+        [SerializeField] private int columns = 7;
+        
+        [GUIColor(0.3f, 0.8f, 0.8f, 1f)]
+        [DisableIf("m_areaType", EAreaType.Collider2D)]
+        [SerializeField] private Vector2 m_spineBoundOffset;
+        
+        [Title("Debug Setting")]
+        [GUIColor(0.9f, 0.24f, 0.38f, 1f)]
+        [SerializeField] private bool m_debugVisible = true;
+        
+        private Vector2 m_boxSize;
+        private Bounds m_spineBounds;
+        private List<Vector2> m_gridCenters = new List<Vector2>();
+        
+      
+     
+        
+        
+        // 마지막으로 알려진 위치, 크기 및 회전을 저장합니다.
+        private Vector2 m_lastPosition;
+        private Vector2 m_lastSize;
+        private Quaternion m_lastRotation;
+        
+        private void Awake()
+        {
+            boxCollider2D = GetComponent<BoxCollider2D>();
+            
+            BoxSizeSetup();
+            DivideBoxColliderArea(m_boxSize);
+            
+            m_lastPosition = transform.position;
+            m_lastSize = m_boxSize;
+            m_lastRotation = transform.rotation;
+        }
+
+        private void Update()
+        {
+            if (HasStateChanged())
+            {
+                UpdateBounds();
+                DivideBoxColliderArea(m_boxSize);
+                UpdateLastKnownState();
+            }
+        }
+        
+        private bool HasStateChanged()
+        {
+            return m_lastPosition != (Vector2)transform.position ||
+                   m_lastSize != m_boxSize ||
+                   m_lastRotation != transform.rotation;
+        }
+        
+        private void UpdateBounds()
+        {
+            switch (m_areaType)
+            {
+                case EAreaType.Collider2D:
+                    m_boxSize = boxCollider2D.bounds.size;
+                    break;
+                case EAreaType.SpineAnimation:
+                    m_spineBounds = GetBoundingBox(skeletonAnimation.Skeleton);
+                    m_boxSize = m_spineBounds.size;
+                    break;
+            }
+        }
+        
+        private void UpdateLastKnownState()
+        {
+            m_lastPosition = transform.position;
+            m_lastSize = m_boxSize;
+            m_lastRotation = transform.rotation;
+        }
+
+        private void BoxSizeSetup()
+        {
+            switch (m_areaType)
+            {
+                case EAreaType.Collider2D:
+                    m_boxSize = boxCollider2D.bounds.size;
+                    break;
+                case EAreaType.SpineAnimation:
+                    m_spineBounds = GetBoundingBox(skeletonAnimation.Skeleton);
+                    m_boxSize = m_spineBounds.size;
+                    break;
+            }
+        }
+        
+        void DivideBoxColliderArea(Vector2 size)
+        {
+            float rectWidth = size.x / columns;
+            float rectHeight = size.y / rows;
+
+            m_gridCenters.Clear();
+
+            for (int i = 0; i < rows; i++)
+            {
+                // 각 행의 하단에 시작점을 정의합니다.
+                Vector2 startPoint;
+                
+                switch(m_areaType)
+                {
+                    case EAreaType.Collider2D:
+                        startPoint = (Vector2)transform.position - size / 2f + new Vector2(0, rectHeight * i+ boxCollider2D.offset.y);
+                        break;
+                    case EAreaType.SpineAnimation:
+                        // 스켈레톤 경계의 최소 y값을 시작점으로 사용
+                        startPoint = new Vector2(transform.position.x + m_spineBounds.center.x -size.x/2f,
+                                         m_spineBounds.center.y - m_spineBounds.extents.y) +
+                                     new Vector2(0, rectHeight * i);
+                        break;
+                    
+                    default: startPoint = Vector2.zero; break;
+                }
+                
+                for (int j = 0; j < columns; j++)
+                {
+                    Vector2 center = startPoint + new Vector2(rectWidth * (j + 0.5f), rectHeight * 0.5f);
+                    m_gridCenters.Add(center);
+                }
+            }
+        }
+        
+        Bounds GetBoundingBox(Skeleton skeleton)
+        {
+            float minX = float.PositiveInfinity, minY = float.PositiveInfinity,
+                maxX = float.NegativeInfinity, maxY = float.NegativeInfinity;
+
+            foreach (Bone bone in skeleton.Bones)
+            {
+                minX = Mathf.Min(minX, bone.WorldX);
+                minY = Mathf.Min(minY, bone.WorldY);
+                maxX = Mathf.Max(maxX, bone.WorldX);
+                maxY = Mathf.Max(maxY, bone.WorldY);
+            }
+            
+            Vector3 size = new Vector3(maxX - minX, maxY - minY, 0) + (Vector3)m_spineBoundOffset;
+            Vector3 center = new Vector3(minX + size.x / 2, minY + size.y / 2, 0);
+            return new Bounds(center, size);
+        }
+        
+        public float GetRectangleWidth()
+        {
+            return m_boxSize.x / columns;
+        }
+
+        public float GetRectangleHeight()
+        {
+            return m_boxSize.y / rows;
+        }
+        
+        public Vector2 GetGridPosition(int row, int column)
+        {
+            // Validate input
+            if (row < 0 || row >= rows || column < 0 || column >= columns)
+            {
+                Debug.LogError("Row or column out of bounds");
+                return Vector2.zero;
+            }
+
+            // Calculate index based on row and column number
+            int index = row * columns + column;
+
+            // Get center point
+            return m_gridCenters[index];
+        }
+
+        void OnDrawGizmos()
+        {
+            if (m_areaType == EAreaType.SpineAnimation)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireCube(gameObject.transform.position + m_spineBounds.center, m_spineBounds.size);
+            }
+            
+            if (m_debugVisible)
+            {
+                BoxSizeSetup();
+                DivideBoxColliderArea(m_boxSize);
+                
+                Gizmos.color = Color.blue;
+                foreach (Vector2 center in m_gridCenters)
+                {
+                    // Draw rectangle
+                    float rectWidth = m_boxSize.x / columns;
+                    float rectHeight = m_boxSize.y / rows;
+                    Vector2 rectPosition = center - new Vector2(rectWidth / 2f, rectHeight / 2f);
+                    Gizmos.DrawWireCube(rectPosition + new Vector2(rectWidth / 2f, rectHeight / 2f),
+                        new Vector3(rectWidth, rectHeight, 0f));
+
+                    // Draw center point
+                    Gizmos.DrawSphere(center, 0.025f);
+                }
+            }
+        }
+        
+    }//NodeArea Class End
+}
